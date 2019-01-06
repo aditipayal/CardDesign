0449d887ea34abc6081a62baeddd7467b98658df
Aditi Agarwal <aditipayalagarwal@gmail.com>
2019-01-06 21:47:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aditi/carddesign/display.java
@@ -0,0 +1,13 @@
+package com.example.aditi.carddesign;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class display extends Activity{
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layer);
+    }
+}
+
